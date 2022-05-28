331e7c59070fd60aefd7ceebaf56e13dd91dd83c
Tom <yungsu2391@gmail.com>
2022-05-28 23:35:37 +0900

    5/28 토 미들웨어 사용 방법

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var fs = require('fs');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 // app.get('/', (req, res) => {res.send('Hello World!')})
 app.get('/', function(request, response) {
@@ -65,19 +68,29 @@ app.get('/create', function(request, response) {
 })
 
 app.post('/create_process',function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
-      })
-  });
+
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+     response.writeHead(302, {Location: `/?id=${title}`});
+     response.end();
+   });
+
+
+  // var body = '';
+  // request.on('data', function(data){
+  //     body = body + data;
+  // });
+  // request.on('end', function(){
+  //     var post = qs.parse(body);
+  //     var title = post.title;
+  //     var description = post.description;
+  //     fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+  //       response.writeHead(302, {Location: `/?id=${title}`});
+  //       response.end();
+  //     })
+  // });
 });
 
 app.get('/update/:pageId',function(request, response){
@@ -108,37 +121,25 @@ app.get('/update/:pageId',function(request, response){
 });
 
 app.post('/update_process',function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(error){
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.redirect(`/page/${title}`);
-        })
-      });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/page/${title}`);
+    })
   });
 });
 
 app.post('/delete_process',function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      console.log(filteredId);
-      fs.unlink(`data/${filteredId}`, function(error){
-        response.redirect('/');
-      })
-  });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  console.log(filteredId);
+  fs.unlink(`data/${filteredId}`, function(error){
+    response.redirect('/');
+  })
 });
 
 
